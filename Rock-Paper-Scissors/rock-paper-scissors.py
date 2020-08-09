3efe36ee464ada58e3bc6aca91da0722822222a9
kpiesk <k.piesk@protonmail.com>
2020-08-09 13:07:56 +0100

    Add Stage 3/5 project
    Now the game is playable indefinitely unless the user enters '!exit'. The program also checks whether the user's input is correct.

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -2,13 +2,29 @@ from random import choice
 
 
 weakness = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
-user_option = input()
-pc_option = choice(list(weakness.keys()))
 
-if user_option in list(weakness.keys()):
+
+# allows the player to play rock-paper-scissors against a computer
+def main():
+    while True:
+        user_option = input()
+        if user_option in list(weakness.keys()):
+            pc_option = choice(list(weakness.keys()))
+            print(game_result(user_option, pc_option))
+        elif user_option == '!exit':
+            print('Bye!')
+            break
+        else:
+            print('Invalid input')
+
+
+# determines the outcome of the game and returns the string of it
+def game_result(user_option, pc_option):
     if weakness[user_option] == pc_option:
-        print(f'Sorry, but computer chose {pc_option}')
+        return f'Sorry, but computer chose {pc_option}'
     elif user_option == pc_option:
-        print(f'There is a draw ({user_option})')
-    else:
-        print(f'Well done. Computer chose {pc_option} and failed')
+        return f'There is a draw ({user_option})'
+    return print(f'Well done. Computer chose {pc_option} and failed')
+
+
+main()
