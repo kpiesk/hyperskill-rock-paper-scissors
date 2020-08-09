794a7526d8dd1ec3675f0696a90d0e62a2eb1fd4
kpiesk <k.piesk@protonmail.com>
2020-08-09 12:59:06 +0100

    Add Stage 2/5 project
    Now the game is fair. Computer chooses a random option, and the program determines who won or whether it's a draw.

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -1,6 +1,14 @@
+from random import choice
+
+
 weakness = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
-
 user_option = input()
-pc_option = weakness[user_option]
+pc_option = choice(list(weakness.keys()))
 
-print(f'Sorry, but computer chose {pc_option}')
+if user_option in list(weakness.keys()):
+    if weakness[user_option] == pc_option:
+        print(f'Sorry, but computer chose {pc_option}')
+    elif user_option == pc_option:
+        print(f'There is a draw ({user_option})')
+    else:
+        print(f'Well done. Computer chose {pc_option} and failed')
