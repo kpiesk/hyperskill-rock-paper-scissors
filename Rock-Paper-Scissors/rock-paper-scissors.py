51574d12954e99a7e5c4a9ba33e72707dbfea741
kpiesk <k.piesk@protonmail.com>
2020-08-09 22:14:43 +0100

    Add Stage 4/5 project
    Now the user is prompted to enter their name, and then the program checks whether there is a record for the user with the same name in a rating.txt file; if yes, then the program uses their score as a starting point for calculating the score in the current game. The user can find out their score by typing '!rating'.

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -1,16 +1,25 @@
 from random import choice
 
 
-weakness = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+WEAKNESS = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+DRAW_SCORE = 50
+WIN_SCORE = 100
+FILE_NAME = 'rating.txt'
 
 
 # allows the player to play rock-paper-scissors against a computer
 def main():
+    user_name = input('Enter your name: ')
+    print(f'Hello, {user_name}')
+    user_rating = get_user_rating(user_name)
+
     while True:
         user_option = input()
-        if user_option in list(weakness.keys()):
-            pc_option = choice(list(weakness.keys()))
-            print(game_result(user_option, pc_option))
+        if user_option in list(WEAKNESS.keys()):
+            pc_option = choice(list(WEAKNESS.keys()))
+            user_rating = game_result(user_option, pc_option, user_rating)
+        elif user_option == '!rating':
+            print(f'Your rating: {user_rating}')
         elif user_option == '!exit':
             print('Bye!')
             break
@@ -18,13 +27,30 @@ def main():
             print('Invalid input')
 
 
-# determines the outcome of the game and returns the string of it
-def game_result(user_option, pc_option):
-    if weakness[user_option] == pc_option:
-        return f'Sorry, but computer chose {pc_option}'
+# checks whether there is a record for the user with the same name in rating.txt
+# if yes, returns the rating from the user's previous game session
+def get_user_rating(user_name):
+    ratings_file = open(FILE_NAME, 'r')
+    user_rating = 0
+
+    for line in ratings_file:
+        if line.split()[0] == user_name:
+            user_rating = int(line.split()[1])
+
+    ratings_file.close()
+    return user_rating
+
+
+# determines the outcome of the game and returns the current user rating
+def game_result(user_option, pc_option, user_rating):
+    if WEAKNESS[user_option] == pc_option:
+        print(f'Sorry, but computer chose {pc_option}')
+        return user_rating
     elif user_option == pc_option:
-        return f'There is a draw ({user_option})'
-    return print(f'Well done. Computer chose {pc_option} and failed')
+        print(f'There is a draw ({user_option})')
+        return user_rating + DRAW_SCORE
+    print(f'Well done. Computer chose {pc_option} and failed')
+    return user_rating + WIN_SCORE
 
 
 main()
